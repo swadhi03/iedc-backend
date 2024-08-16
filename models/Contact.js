57bd1f85a9363cfbe6866636f3bf75c2aa2385ef
swadhi03 <swathiksanthosh03@gmail.com>
2024-08-17 00:00:47 +0530

    completed

--- models/Contact.js
+++ models/Contact.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose")
 const ContactSchema = mongoose.Schema(
     {
         name:{type:String},
-        email:{type:String},
-        subject:{type:String}
+        email:{type:String,required:true},
+        subject:{type:String},
+        message:{type:String}
     }
 )
 let ContactModel = mongoose.model("contact",ContactSchema)
